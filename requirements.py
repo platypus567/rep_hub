6b3051f30ce82d5e0e3a5e3d9c9e99887f4665df
Luke <87561450+OperationNorthwoods@users.noreply.github.com>
2023-02-08 16:05:02 -0500

    added python venv
    added python venv, or 'virtual environment'. the purpose of this venv is so the user doesn't have to have a specific python package and package versions needed for the app installed on their local machine.

---
+++ requirements.py
@@ -0,0 +1,2 @@
+required = ['click==8.1.3', 'colorama == 0.4.5', 'dominate == 2.6.0', 'Flask == 2.1.2', 'Flask-Bootstrap == 3.3.7.1', 'Flask-WTF == 1.0.1', 'itsdangerous == 2.1.2',
+ 'Jinja2 == 3.1.2', 'MarkupSafe == 2.1.1', 'visitor == 0.1.3', 'Werkzeug == 2.1.2', 'WTForms==2.2.1', 'wtforms-widgets==1.0.6', 'requests~=2.28.1']
